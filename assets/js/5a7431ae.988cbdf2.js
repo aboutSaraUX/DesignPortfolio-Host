c391dfe0f4f33e1d6d02f02e90c93c2304739bd3
dbqt <6835629+dbqt@users.noreply.github.com>
2025-11-25 08:25:53 +0000

    Deploy website - based on c86b971ca0f376473b544fc6ce9d78b778a990d9

---
+++ assets/js/5a7431ae.988cbdf2.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunksara_site=globalThis.webpackChunksara_site||[]).push([[790],{8779:a=>{a.exports=JSON.parse('{"metadata":{"permalink":"/DesignPortfolio-Host/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
--- assets/js/acecf23e.07875ad6.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunksara_site=globalThis.webpackChunksara_site||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/designportfolio-host/blog","blogTitle":"Blog","authorsListPath":"/designportfolio-host/blog/authors"}')}}]);
